f8fc492b23dcceb30cf55198b0380b49ef9c73c8
Janet <jbembe12@yahoo.com>
2020-05-11 13:14:38 +0200

    Test for Corentin

--- TrocEnchere.org/src/fr/eni/projet01/trocenchere/PourTester.java
+++ TrocEnchere.org/src/fr/eni/projet01/trocenchere/PourTester.java
@@ -7,6 +7,7 @@ public class PourTester {
 		System.out.println("C'est Coco");
 		System.out.println("C'est aussi Vincent");
 		System.out.println("it's me again");
+		System.out.println("You did it Vincent!");
 	}
 
 }
