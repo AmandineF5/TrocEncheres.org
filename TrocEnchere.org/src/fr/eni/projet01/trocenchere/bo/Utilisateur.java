00540cf67a0d4b5d30c9662b1b0502f22418d382
Corentin Villiermet <corentin.villiermet@gmail.com>
2020-05-12 10:27:57 +0200

    fix constructeur Utilisateur
    remove n°utilisateur

--- TrocEnchere.org/src/fr/eni/projet01/trocenchere/bo/Utilisateur.java
+++ TrocEnchere.org/src/fr/eni/projet01/trocenchere/bo/Utilisateur.java
@@ -32,6 +32,22 @@ public class Utilisateur {
 		this.administrateur = false;
 	}
 	
+	//constructeur sans n°UTILISATEUR -> généré automatiquement en entrant dans la BDD
+	public Utilisateur(String pseudo, String nom, String prenom, String email, String telephone,
+			String rue, String codePostal, String ville, String motDePasse, float credit, boolean administrateur) {
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.motDePasse = motDePasse;
+		this.credit = credit;
+		this.administrateur = administrateur;
+	}
+	
 	//constructeur ENTIER
 	public Utilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
 			String rue, String codePostal, String ville, String motDePasse, float credit, boolean administrateur) {
