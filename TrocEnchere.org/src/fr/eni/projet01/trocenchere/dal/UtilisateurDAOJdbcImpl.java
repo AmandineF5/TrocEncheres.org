a6fa96678d778d75ece31cf38cdd7b6dbd6f6691
Janet <jbembe12@yahoo.com>
2020-05-12 13:33:29 +0200

    DAOJdbc user

--- TrocEnchere.org/src/fr/eni/projet01/trocenchere/dal/UtilisateurDAOJdbcImpl.java
+++ TrocEnchere.org/src/fr/eni/projet01/trocenchere/dal/UtilisateurDAOJdbcImpl.java
@@ -23,7 +23,7 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO{
 	public void insertUser(Utilisateur user) {
 		try {
 			Connection cnx = fr.eni.projet01.trocenchere.dal.Connection.getConnection();
-			//pool Connection cnx = ConnectionProvider.getConnection();
+			//Connection cnx = ConnectionProvider.getConnection();
 			PreparedStatement state;
 			ResultSet rs;
 			//préparer le statement
@@ -54,25 +54,91 @@ public class UtilisateurDAOJdbcImpl implements UtilisateurDAO{
 			e.printStackTrace();
 			//cnx.rollback();
 		}
-	
-	
+
 	}
 
 	@Override
 	public Utilisateur selectByIdUser(int noUtilisateur) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Utilisateur user = new Utilisateur();
+		try {
+			Connection cnx = ConnectionProvider.getConnection();
+			PreparedStatement state;
+			ResultSet rs;
+			//préparer le statement
+			state = cnx.prepareStatement(SELECTBYID_USER_SQL);
+			state.setInt(1, noUtilisateur);
+			rs = state.executeQuery();
+			rs.next();
+			user.setNoUtilisateur(noUtilisateur);
+			user.setPseudo(rs.getString("pseudo"));
+			user.setNom(rs.getString("nom"));
+			user.setPrenom(rs.getString("prenom"));
+			user.setEmail(rs.getString("email"));
+			user.setTelephone(rs.getString("telephone"));
+			user.setRue(rs.getString("rue"));
+			user.setCodePostal(rs.getString("code_postal"));
+			user.setVille(rs.getString("ville"));
+			user.setMotDePasse(rs.getString("mot_de_passe"));
+			user.setCredit(rs.getInt("credit"));		
+					
+			rs.close();
+			state.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return user;
 	}
 
 	@Override
-	public void updateUser(Utilisateur utilisateur) {
-		// TODO Auto-generated method stub
+	public void updateUser(Utilisateur user) {
+		Connection cnx;
+		try {
+			cnx = ConnectionProvider.getConnection();
+			PreparedStatement state;
+			//préparer le statement
+			state = cnx.prepareStatement(UPDATE_USER_SQL, PreparedStatement.RETURN_GENERATED_KEYS);
+			state.setString(1, user.getPseudo());
+			state.setString(2, user.getNom());
+			state.setString(3, user.getPrenom());
+			state.setString(4, user.getEmail());
+			state.setString(5, user.getTelephone());
+			state.setString(6, user.getRue());
+			state.setString(7, user.getCodePostal());
+			state.setString(8, user.getVille());
+			state.setString(9, user.getMotDePasse());
+			state.setFloat(10, user.getCredit());
+			state.setBoolean(11, user.isAdministrateur());
+			state.setInt(12, user.getNoUtilisateur());
+			state.execute();
+			state.close();
+			cnx.commit();
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public void deleteUser(int noUtilisateur) {
-		// TODO Auto-generated method stub
+	
+			try {
+				Connection cnx = ConnectionProvider.getConnection();
+				PreparedStatement state;
+				state = cnx.prepareStatement(DELETE_USER_SQL);
+				state.setInt(1, noUtilisateur);
+				state.executeUpdate();
+				state.close();
+				cnx.commit();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		
 	}
 
