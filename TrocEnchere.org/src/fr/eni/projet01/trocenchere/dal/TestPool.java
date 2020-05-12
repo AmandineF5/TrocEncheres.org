62caeb6ae00fa7a3691f910a395ea3ff2753c580
Janet <jbembe12@yahoo.com>
2020-05-12 14:46:17 +0200

    pool test

---
+++ TrocEnchere.org/src/fr/eni/projet01/trocenchere/dal/TestPool.java
@@ -0,0 +1,52 @@
+package fr.eni.projet01.trocenchere.dal;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class TestPool
+ */
+@WebServlet("/TestPool")
+public class TestPool extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+
+        try {
+             Context context = new InitialContext();
+            DataSource dataSource = (DataSource) context.lookup("java:comp/env/jdbc/pool_cnx");
+            Connection cnx = dataSource.getConnection();
+            out.print("La connexion est " + (cnx.isClosed()?"fermée":"ouverte")+".");
+            cnx.close();
+
+
+        } catch (NamingException | SQLException e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            out.println("erreur bdd" + e.getMessage());
+        }
+
+
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+
+}
