0d2a83e87dc10f4dd2e29ac85a2a9d75284d8f0c
Amandine Foulet <amandine.foulet@gmail.com>
2020-05-13 17:13:32 +0200

    fix change name ServletException to IhmException

---
+++ TrocEnchere.org/src/fr/eni/projet01/trocenchere/erreurs/IhmException.java
@@ -0,0 +1,30 @@
+package fr.eni.projet01.trocenchere.erreurs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IhmException extends Exception {
+	private static final long serialVersionUID = 1L;
+	private List<String> ServletCodesErreurs;
+	
+	public IhmException() {
+		super();
+		this.ServletCodesErreurs=new ArrayList<>();
+	}
+	
+	
+	public void ajouterErreur(String err){
+		if(!this.ServletCodesErreurs.contains(err))
+		{
+			this.ServletCodesErreurs.add(err);
+		}
+	}
+	
+	public boolean verifieErreurs(){
+		return this.ServletCodesErreurs.size()>0;
+	}
+	
+	public List<String> getListeErreur(){
+		return this.ServletCodesErreurs;
+	}
+}
