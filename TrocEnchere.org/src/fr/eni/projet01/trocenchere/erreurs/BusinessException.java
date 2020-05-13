aedcfe0a8e4696acd3a311a6e59c117cfd6e5c77
Amandine Foulet <amandine.foulet@gmail.com>
2020-05-13 11:47:29 +0200

    add classes for errors management

---
+++ TrocEnchere.org/src/fr/eni/projet01/trocenchere/erreurs/BusinessException.java
@@ -0,0 +1,31 @@
+package fr.eni.projet01.trocenchere.erreurs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusinessException extends Exception {
+	private static final long serialVersionUID = 1L;
+	private List<String> BusinessCodesErreurs;
+	
+	public BusinessException() {
+		super();
+		this.BusinessCodesErreurs=new ArrayList<>();
+	}
+	
+	
+	public void ajouterErreur(String err){
+		if(!this.BusinessCodesErreurs.contains(err))
+		{
+			this.BusinessCodesErreurs.add(err);
+		}
+	}
+	
+	public boolean verifieErreurs(){
+		return this.BusinessCodesErreurs.size()>0;
+	}
+	
+	public List<String> getListeErreur(){
+		return this.BusinessCodesErreurs;
+	}
+
+}
