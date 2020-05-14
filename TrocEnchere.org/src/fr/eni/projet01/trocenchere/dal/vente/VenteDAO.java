82f6a163c7c06d18727784ad7cd1427187b5f182
Corentin Villiermet <corentin.villiermet@gmail.com>
2020-05-14 11:56:07 +0200

    fix VenteDAO
    remove comments

--- TrocEnchere.org/src/fr/eni/projet01/trocenchere/dal/vente/VenteDAO.java
+++ TrocEnchere.org/src/fr/eni/projet01/trocenchere/dal/vente/VenteDAO.java
@@ -3,31 +3,31 @@ package fr.eni.projet01.trocenchere.dal.vente;
 import java.util.List;
 
 import fr.eni.projet01.trocenchere.bo.Vente;
+import fr.eni.projet01.trocenchere.erreurs.BusinessException;
 
 public interface VenteDAO {
+	
+	//manque les "throws Exception"
+	
 	/**
 	 * création d'un article à vendre
 	 * @param Vente
 	 */
 	public void insert (Vente Vente);
-	//INSERT INTO ventes(nomarticle, description, date_fin_encheres, prix_initial, no_utilisateur, no_categorie) VALUES (?,?,?,?,?,?)
-	//INSERT INTO retraits(no_vente, rue, code_postal, ville) VALUES (?,?,?,?)
-		
+	
 	/**
 	 * affichage de la vente par noVente
 	 * @param noVente
 	 * @return une vente
 	 */
 	public Vente selectById (int noVente);
-	//SELECT * FROM ventes JOIN retraits ON ventes.no_vente = retraits.no_vente WHERE no_vente = ?
-		
+	
 	/**
 	 * Récupère toutes les ventes publiées par un utilisateur
 	 * @param noUtilisateur
 	 * @return la liste des ventes appartenant à l'utilisateur
 	 */
 	public  List<Vente> selectAll (int noUtilisateur);
-	//SELECT * FROM ventes JOIN retraits ON ventes.no_vente = retraits.no_vente WHERE no_utilisateur = ?
 	
 	/**
 	 * Chercher et récupère toutes les ventes publiées comportant le mot clé
@@ -35,22 +35,19 @@ public interface VenteDAO {
 	 * @return une liste de vente
 	 */
 	public List<Vente> searchByKeyWord (String keyWord);
-	//SELECT * FROM ventes JOIN retraits ON ventes.no_vente = retraits.no_vente WHERE nomarticle LIKE ? OR description LIKE ?
-		
+	
 	/**
 	 * cherche et récupère une vente par categorie
 	 * @param libelle ou noCategorie
 	 * @return une liste de vente
 	 */
 	public List<Vente> searchByCatagory (String libelle); //ou par int noCategorie
-	//SELECT * FROM ventes JOIN retraits ON ventes.no_vente = retraits.no_vente WHERE no_categorie/libelle=?
 	
 	/**
 	 * suppression d'une vente
 	 * @param noVente
+	 * @throws BusinessException 
 	 */
-	public void delete (int noVente);
-	//DELETE FROM ventes WHERE no_vente = ?
-	//DELETE FROM retraits WHERE no_vente=?
+	public void delete (int noVente) throws BusinessException;
 	
 }
