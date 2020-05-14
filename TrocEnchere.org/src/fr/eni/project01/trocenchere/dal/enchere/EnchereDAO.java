bcec495ce7e8e336f509ab93c3312804d9dc5be7
Janet <jbembe12@yahoo.com>
2020-05-14 16:39:26 +0200

    interface DAO enchere

--- TrocEnchere.org/src/fr/eni/project01/trocenchere/dal/enchere/EnchereDAO.java
+++ TrocEnchere.org/src/fr/eni/project01/trocenchere/dal/enchere/EnchereDAO.java
@@ -1,5 +1,15 @@
 package fr.eni.project01.trocenchere.dal.enchere;
 
+
+import java.util.List;
+
+import fr.eni.projet01.trocenchere.bo.Enchere;
+import fr.eni.projet01.trocenchere.erreurs.BusinessException;
+
 public interface EnchereDAO {
 
+	public Enchere selectByUserId(int noUtilisateur) throws BusinessException;
+	public List<Enchere> selectByVenteId(int noVente) throws BusinessException;
+	public Enchere insert(Enchere newEnchere) throws BusinessException;
+	public void delete(int noVente) throws BusinessException;
 }
