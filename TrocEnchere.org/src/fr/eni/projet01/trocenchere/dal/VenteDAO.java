b64f4e8edfd13069186623ccfb08cf52fd491cdb
Leslie <leslie.bileckot@gmail.com>
2020-05-14 09:15:33 +0200

    modify venteDAO interface

--- TrocEnchere.org/src/fr/eni/projet01/trocenchere/dal/VenteDAO.java
+++ TrocEnchere.org/src/fr/eni/projet01/trocenchere/dal/VenteDAO.java
@@ -10,30 +10,47 @@ public interface VenteDAO {
 	 * @param Vente
 	 */
 	public void insert (Vente Vente);
-	
+	//INSERT INTO ventes(nomarticle, description, date_fin_encheres, prix_initial, no_utilisateur, no_categorie) VALUES (?,?,?,?,?,?)
+	//INSERT INTO retraits(no_vente, rue, code_postal, ville) VALUES (?,?,?,?)
+		
 	/**
 	 * affichage de la vente par noVente
 	 * @param noVente
 	 * @return une vente
 	 */
 	public Vente selectById (int noVente);
-	
+	//SELECT * FROM ventes JOIN retraits ON ventes.no_vente = retraits.no_vente WHERE no_vente = ?
+		
 	/**
 	 * Récupère toutes les ventes publiées par un utilisateur
 	 * @param noUtilisateur
 	 * @return la liste des ventes appartenant à l'utilisateur
 	 */
 	public  List<Vente> selectAll (int noUtilisateur);
+	//SELECT * FROM ventes JOIN retraits ON ventes.no_vente = retraits.no_vente WHERE no_utilisateur = ?
 	
 	/**
-	 * Récupère toutes les ventes publiées comportant le mot clé
+	 * Chercher et récupère toutes les ventes publiées comportant le mot clé
 	 * @param keyWord
 	 * @return une liste de vente
 	 */
 	public List<Vente> searchByKeyWord (String keyWord);
+	//SELECT * FROM ventes JOIN retraits ON ventes.no_vente = retraits.no_vente WHERE nomarticle LIKE ? OR description LIKE ?
+		
+	/**
+	 * cherche et récupère une vente par categorie
+	 * @param libelle ou noCategorie
+	 * @return une liste de vente
+	 */
+	public List<Vente> searchByCatagory (String libelle); //ou par int noCategorie
+	//SELECT * FROM ventes JOIN retraits ON ventes.no_vente = retraits.no_vente WHERE no_categorie/libelle=?
 	
-	
-	
-	
+	/**
+	 * suppression d'une vente
+	 * @param noVente
+	 */
+	public void delete (int noVente);
+	//DELETE FROM ventes WHERE no_vente = ?
+	//DELETE FROM retraits WHERE no_vente=?
 	
 }
