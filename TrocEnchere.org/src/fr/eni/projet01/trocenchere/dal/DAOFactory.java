1c4c884c5608feaf4e7db9bc582be196b34c6bc8
Corentin Villiermet <corentin.villiermet@gmail.com>
2020-05-14 13:59:41 +0200

    fix DAOFactory
    now working =) !

--- TrocEnchere.org/src/fr/eni/projet01/trocenchere/dal/DAOFactory.java
+++ TrocEnchere.org/src/fr/eni/projet01/trocenchere/dal/DAOFactory.java
@@ -1,16 +1,18 @@
 package fr.eni.projet01.trocenchere.dal;
 
 import fr.eni.projet01.trocenchere.dal.vente.VenteDAO;
+import fr.eni.projet01.trocenchere.dal.vente.VenteDAOJdbcImpl;
 
 public class DAOFactory {
 
 	public static UtilisateurDAO getUtilisateurDAO() {
 		return new UtilisateurDAOJdbcImpl();
+		}
 		
 		
 	public static VenteDAO getVenteDAO() {
 		return new VenteDAOJdbcImpl();
-	}
+		}
 		
 	}
-}
+
