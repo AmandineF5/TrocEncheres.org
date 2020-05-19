cbde27be9c03abd40ddc8af4850a4d1f7dee86ed
Janet <jbembe12@yahoo.com>
2020-05-19 14:55:59 +0200

    DAL Enchere

--- TrocEnchere.org/src/fr/eni/projet01/trocenchere/bll/EnchereManager.java
+++ TrocEnchere.org/src/fr/eni/projet01/trocenchere/bll/EnchereManager.java
@@ -15,92 +15,91 @@ import fr.eni.projet01.trocenchere.erreurs.BusinessException;
 public class EnchereManager {
 	EnchereDAO enchereDAO;
 	Enchere enchere;
-	
+
 	public EnchereManager() {
 		this.enchereDAO = DAOFactory.getEnchereDAO();
 	}
 
-	public void ajouterEnchere (Enchere enchere) throws BusinessException {
+	public void ajouterEnchere(Enchere enchere) throws BusinessException {
 		int noUser = enchere.getEncherit().getNoUtilisateur();
 		int noSale = enchere.getConcerne().getNoVente();
 		Enchere test = selectEnchereByUserIdEtNoVente(noUser, noSale);
 		if (test != null) {
-		enchereDAO.updateEnchere(enchere);
-		}else
-		this.enchereDAO.insert(enchere);
-		Integer points = enchere.getPoints();
-		Vente vente = enchere.getConcerne();
-		int noVente = vente.getNoVente();
-		Enchere meilleureEnchere = this.trouverHighestBid(noVente);
-		Integer pointsMeilleureEnchere = meilleureEnchere.getPoints();
-		
-		if (points > pointsMeilleureEnchere) {
-			VenteManager vm = new VenteManager();
-			vm.mettreAJourPrixVente (noVente, points);
+			enchereDAO.updateEnchere(enchere);
+		} else {
+			this.enchereDAO.insert(enchere);
+			Integer points = enchere.getPoints();
+			Vente vente = enchere.getConcerne();
+			int noVente = vente.getNoVente();
+			Enchere meilleureEnchere = this.trouverHighestBid(noVente);
+			Integer pointsMeilleureEnchere = meilleureEnchere.getPoints();
+
+			if (points > pointsMeilleureEnchere) {
+				VenteManager vm = new VenteManager();
+				vm.mettreAJourPrixVente(noVente, points);
+			}
 		}
-		
+
 	}
-	
-	//varification que il n'y a pas deja un enchere par l'utilisateur pour ce produit
-	public Enchere selectEnchereByUserIdEtNoVente (int noUtilisateur, int noVente) throws BusinessException{
-		
+
+	// varification que il n'y a pas deja un enchere par l'utilisateur pour ce
+	// produit
+	public Enchere selectEnchereByUserIdEtNoVente(int noUtilisateur, int noVente) throws BusinessException {
+
 		return enchereDAO.selectOneByUserIdVenteId(noUtilisateur, noVente);
 	}
 
-	public  List<Enchere> selectionnerEnchereByIdUser (int noUtilisateur) throws BusinessException {
+	public List<Enchere> selectionnerEnchereByIdUser(int noUtilisateur) throws BusinessException {
 		List<Enchere> listeEnchere = new ArrayList<Enchere>();
 		listeEnchere = enchereDAO.selectByUserId(noUtilisateur);
 		return listeEnchere;
 	}
 
-	//Don't understand this function? why not call the selectbyventeId directly
-	public  List<Enchere> selectionnerEnchereByIdVente (int noVente) throws BusinessException {
+	// Don't understand this function? why not call the selectbyventeId directly
+	public List<Enchere> selectionnerEnchereByIdVente(int noVente) throws BusinessException {
 		List<Enchere> listeEnchere = new ArrayList<Enchere>();
 		listeEnchere = enchereDAO.selectByVenteId(noVente);
 		return listeEnchere;
 	}
-	
-	public void supprimerEnchere (int noVente) throws BusinessException {
+
+	public void supprimerEnchere(int noVente) throws BusinessException {
 		this.enchereDAO.delete(noVente);
 	}
-	
-	public String trouverClassementEnchere (int noVente, int noUtilisateurSession) throws BusinessException {
-		String classement=null;
+
+	public String trouverClassementEnchere(int noVente, int noUtilisateurSession) throws BusinessException {
+		String classement = null;
 		List<Enchere> listeEnchere = new ArrayList<Enchere>();
 		listeEnchere = this.selectionnerEnchereByIdVente(noVente);
 		int nbEnchere = listeEnchere.size();
 		int noUtilisateurEnchere;
 		Utilisateur utilisateurEnchere;
 		int index = 0;
-		
-		for(Enchere e : listeEnchere) {
+
+		for (Enchere e : listeEnchere) {
 			utilisateurEnchere = e.getEncherit();
 			noUtilisateurEnchere = utilisateurEnchere.getNoUtilisateur();
-			
-			if (noUtilisateurEnchere==noUtilisateurSession) {
+
+			if (noUtilisateurEnchere == noUtilisateurSession) {
 				break;
 			}
-			
-			index++;		
+
+			index++;
 		}
-		
-		classement = String.format("%d / %d", index, nbEnchere);		
+
+		classement = String.format("%d / %d", index, nbEnchere);
 		return classement;
 	}
-	
-	public Enchere trouverHighestBid (int noVente) throws BusinessException {
-		Enchere highestBid=null;
+
+	public Enchere trouverHighestBid(int noVente) throws BusinessException {
+		Enchere highestBid = null;
 		List<Enchere> listeEnchere = new ArrayList<Enchere>();
 		listeEnchere = enchereDAO.selectByVenteId(noVente);
-		for(Enchere e : listeEnchere) {
+		for (Enchere e : listeEnchere) {
 			System.out.println(e);
 		}
-		//problem here out of bounds exception
-		highestBid = listeEnchere.get(0);	
+		// problem here out of bounds exception
+		highestBid = listeEnchere.get(0);
 		return highestBid;
 	}
-	
-	
-	
-	
+
 }
