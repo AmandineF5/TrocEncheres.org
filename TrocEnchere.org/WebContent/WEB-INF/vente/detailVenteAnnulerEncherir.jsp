8856eab4aad1d561f4f92a3794f54a284c9b3a8b
Janet <jbembe12@yahoo.com>
2020-05-20 12:29:40 +0200

    Detail vente annuler enchere

---
+++ TrocEnchere.org/WebContent/WEB-INF/vente/detailVenteAnnulerEncherir.jsp
@@ -0,0 +1,147 @@
+
+<!-- JANET -->
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css">
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+	integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+	crossorigin="anonymous">
+<link rel="stylesheet" href="CSS/main.css">
+<script defer
+	src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+<!-- created by Janet -->
+<title>Troc-Enchères Détail Vente</title>
+</head>
+<body>
+	<%@ include file="../fragments/header.jsp" %>
+	<main>
+		<div class="subtitle is-medium">Détail Vente</div>
+
+		<div class="field is-horizontal">
+			<div class="field-label is-normal">
+				<label class="label">Article:</label>
+			</div>
+			<div class="field-body">
+				<div class="field">${vente.nomArticle}</div>
+			</div>
+		</div>
+
+		<div class="field is-horizontal">
+			<img
+				src="${pageContext.request.contextPath}/nomdupath/${vente.nomImage}"
+				alt="Photo d'objet à vendre">
+		</div>
+
+		<div class="field is-horizontal">
+			<div class="field-label is-normal">
+				<label class="label">Meilleure offre:</label>
+			</div>
+			<div class="field-body">
+				<div class="field">
+					<c:choose>
+						<c:when test="${enchere.points == null}">
+   			 			0
+  						</c:when>
+						<c:otherwise>
+   						 ${enchere.points} pts par ${enchere.getEncherit().getPseudo()}
+  						</c:otherwise>
+					</c:choose>
+
+
+				</div>
+			</div>
+		</div>
+
+		<div class="field is-horizontal">
+			<div class="field-label is-normal">
+				<label class="label">Mise à prix:</label>
+			</div>
+			<div class="field-body">${vente.miseAPrix}</div>
+		</div>
+
+		<div class="field is-horizontal">
+			<div class="field-label is-normal">
+				<label class="label">Fin de l'enchère:</label>
+			</div>
+			<div class="field-body">
+				<div class="field">${vente.dateFinEncheres}</div>
+			</div>
+		</div>
+
+		<div class="field is-horizontal">
+			<div class="field-label is-normal">
+				<label class="label">Retrait:</label>
+			</div>
+			<div class="field-body">
+				<div class="field">
+					${vente.getLieuRetrait().getRue()}<br />
+					${vente.getLieuRetrait().getCodePostal()}
+					${vente.getLieuRetrait().getVille()}
+
+				</div>
+			</div>
+		</div>
+
+		<div class="field is-horizontal">
+			<div class="field-label is-normal">
+				<label class="label">Vendeur:</label>
+			</div>
+			<div class="field-body">
+				<a
+					href="${pageContext.request.contextPath}/afficher-compte?noUtilisateur=${vente.vendeur.noUtilisateur}">
+						${vente.vendeur.pseudo}</a>
+			</div>
+		</div>
+
+		
+				<c:if test="${message!=null && message.length()>0 }">
+					<p>${message}</p>
+				</c:if>
+
+		<form action="/TrocEnchere.org/DetailVenteAnnulerEncherir" method="post">
+			<div class="field is-horizontal">
+				<div class="field-label is-normal">
+					<label class="label">Ma proposition:</label>
+				</div>
+				<div class="field-body">
+					<div class="field">
+						<input type="number" class="input" name="encherir"
+							max="${utilisateur.credit}" min="${enchere.points+1}"
+							value="${enchere.points+1}"> 
+							<input type="hidden"
+							name="venteID" value="${vente.noVente}">
+
+					</div>
+
+				</div>
+				<div class="field-body">
+					<div class="field">
+						<div class="control">
+							<button type="submit" class="button is-primary" name="encherir">Enchérir</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</form>
+
+
+		<div>
+		<form action="/TrocEnchere.org/DetailVenteAnnulerEncherir" method="post">
+			<button type="submit" class="button is-primary" name="annuler" value="${enchere.points}">Annuler la dernière enchère</button>
+		</form>
+		<a href="${pageContext.request.contextPath}/Accueil"><button type="submit"
+				class="button is-primary is-light" name="bouton" value="retour">Back</button></a>
+		</div>
+	</main>
+<%@ include file="../fragments/script.html" %>
+</body>
+</html>
---
+++ TrocEnchere.org/src/fr/eni/projet01/trocenchere/ihm/vente/DetailVenteAnnulerEncherir.java
@@ -0,0 +1,184 @@
+package fr.eni.projet01.trocenchere.ihm.vente;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.projet01.trocenchere.bll.EnchereManager;
+import fr.eni.projet01.trocenchere.bll.UtilisateurManager;
+import fr.eni.projet01.trocenchere.bll.VenteManager;
+import fr.eni.projet01.trocenchere.bo.Enchere;
+import fr.eni.projet01.trocenchere.bo.Utilisateur;
+import fr.eni.projet01.trocenchere.bo.Vente;
+import fr.eni.projet01.trocenchere.erreurs.BusinessException;
+
+/**
+ * Servlet implementation class DetailVenteAnnulerEncherir
+ * corresponds to maquette 7
+ * Janet
+ */
+@WebServlet("/DetailVenteAnnulerEncherir")
+public class DetailVenteAnnulerEncherir extends HttpServlet {
+       
+	private static final long serialVersionUID = 1L;
+	VenteManager vM = new VenteManager();
+    EnchereManager eM = new EnchereManager();
+    UtilisateurManager uM = new UtilisateurManager();
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+				HttpSession session = request.getSession();
+				//get noVente from incoming page which should arrive in a parameter
+				//getting the id
+				
+				//test 
+				int noVenteAAfficher= 8;
+				
+				//Real code
+				//int noVenteAAfficher = Integer.parseInt(request.getParameter("noVente"));
+					
+				//find the Vente in the database
+				Vente venteAAfficher = new Vente();
+				try {
+					
+					 venteAAfficher = vM.selectionnerVenteById(noVenteAAfficher);
+					 
+				} catch (BusinessException e) {
+					e.printStackTrace();
+					request.setAttribute("listeCodesErreur", e.getListeErreur());
+				}
+				//Set vente in session for the post
+				session.setAttribute("VenteConcerne", venteAAfficher);
+				
+				//set vente as attribute
+				request.setAttribute("vente", venteAAfficher);
+				
+				//find the highest bid by id no
+				Enchere highestEnchere = new Enchere();
+
+				try {
+					
+					highestEnchere = eM.trouverHighestBid(noVenteAAfficher);
+
+				} catch (BusinessException e) {
+					e.printStackTrace();
+					request.setAttribute("listeCodesErreur", e.getListeErreur());
+				}
+				//set bid as attribute
+				
+				request.setAttribute("enchere", highestEnchere);
+				
+				//get the current user id from session
+				Utilisateur user = (Utilisateur) session.getAttribute("utilisateur");
+				//send user to the jsp
+				request.setAttribute("utilisateur", user);
+				
+				String message = null;
+				//notify user if they are currently the highest bidder
+				if(user.getNoUtilisateur() == highestEnchere.getEncherit().getNoUtilisateur()) {
+					message = "Vous avez actuellement la meilleure enchére";
+					request.setAttribute("message", message);
+				}
+				
+				//block bidding if credit is not ok
+				if(user.getCredit()<highestEnchere.getPoints()) {
+					message = "Vous n'avez pas assez de credit";
+		            request.setAttribute("message", message);
+				}
+				
+				
+				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/vente/detailVenteAnnulerEncherir.jsp");
+				rd.forward(request, response);
+				
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = null;
+		HttpSession session = request.getSession();
+		
+		//get user
+		Utilisateur user = (Utilisateur) session.getAttribute("utilisateur");
+		//get Vent
+		Vente venteConcerne = (Vente) session.getAttribute("VenteConcerne");
+		
+		if (request.getParameter("annuler")!=null) {
+			
+			//delete bid
+			try {
+				eM.deleteUserbid(venteConcerne.getNoVente(), user.getNoUtilisateur());
+				
+			} catch (BusinessException e) {
+				e.printStackTrace();
+			}
+			
+			//remettre le credit
+			//prendre le prix de l'enchere
+			int bid = Integer.parseInt(request.getParameter("annuler"));
+			//calculate
+			int newCredit = user.getCredit() + bid;
+			//enlever le credit de l'utilisateur
+			user.setCredit(newCredit);
+			//send user with new credit to be updated
+			try {
+				uM.mettreAJourUtilisateur(user);
+				//update in session
+				session.setAttribute("utilisateur", user);
+			} catch (BusinessException e) {
+				e.printStackTrace();
+			}
+			
+			//send to accueil
+			response.sendRedirect(request.getContextPath()+"/Accueil");
+		}
+		
+		if (request.getParameter("encherir")!=null) {
+		//get bid from jsp
+		int bid = Integer.parseInt(request.getParameter("encherir"));
+		//get local date
+		LocalDate date = LocalDate.now();
+		//new bid
+		Enchere newEnchere = new Enchere(date, venteConcerne, user, bid);
+		//insert into db
+		try {
+			eM.ajouterEnchere(newEnchere);
+		} catch (BusinessException e) {
+			e.printStackTrace();
+			request.setAttribute("listeCodesErreur", e.getListeErreur());
+		}
+		//CREDIT UPDATE
+		//calculate
+		int newCredit = user.getCredit() - bid;
+		//enlever le credit de l'utilisateur
+		user.setCredit(newCredit);
+		//send user with new credit to be updated
+		try {
+			uM.mettreAJourUtilisateur(user);
+			//update in session
+			session.setAttribute("utilisateur", user);
+		} catch (BusinessException e) {
+			e.printStackTrace();
+		}		
+		//set noVente in parameters to be sent to the next page
+		//get noVente
+		int venteID = venteConcerne.getNoVente();
+		request.setAttribute("noVente", venteID);
+		
+		rd = request.getRequestDispatcher("/DetailVenteAnnulerEncherir");
+		rd.forward(request, response);
+		}
+	}
+
+}
